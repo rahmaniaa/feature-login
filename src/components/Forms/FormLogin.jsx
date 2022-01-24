4a921a8558fded073e5c37316e67b45aea36ca46
rahmaniaa <kopirahma.putri@gmail.com>
2022-01-24 11:28:30 +0700

    done

--- src/components/Forms/FormLogin.jsx
+++ src/components/Forms/FormLogin.jsx
@@ -1,79 +1,67 @@
-import { useFormik } from 'formik';
-import { Form, Button } from 'react-bootstrap';
+import { Formik, Form, Field} from 'formik';
+import { Button } from 'react-bootstrap';
 import *as Yup from 'yup';
 
+const LoginSchema = Yup.object().shape({
+    email: Yup.string().email('Invalid email').required('Required'),
+    password: Yup.string().required('Required')
+        .matches(/^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{6,}$/,
+        "Must Contain 6 Characters, One Uppercase, One Lowercase, One Number and one special case Character"
+    ),
+});
+
 export default function FormLogin () {
-    const formik = useFormik({
-        initialValues:{
-            email:"",
-            password:"",
-        },  
-
-        validationSchema: Yup.object({
-            email:Yup.string()
-                .email("Invalid email address")
-                .required("email harus diisi"),
-            password:Yup.string()
-                .required('Please Enter your password')
-                .matches(/^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{6,}$/,
-                "Must Contain 6 Characters, One Uppercase, One Lowercase, One Number and one special case Character"
-            ),
-
-        }),
-
-        onSubmit:values => {
-            alert(JSON.stringify(values, null, 2));
-        },
-        
-    })
-
 
     return (
-      <div className="d-flex justify-content-center align-items-center vh-100">
-        <div className="form-login">
-            <h1 className="mb-5">Welcome back!</h1>
-            <Form onSubmit={formik.handleSubmit} className="w-100">
-                <Form.Group>
-                    <Form.Control
-                        className="mb-4"
-                        type="text"
-                        placeholder="Email"
-                        name="email"
-                        
-                        // {...formik.getFieldProps('email')}
-                        onChange={formik.handleChange}
-                    />
-                    {formik.touched.email 
-                      && formik.errors.email 
-                      &&
-                      <Form.Control.Feedback type="invalid">
-                      {formik.errors.email}
-                      </Form.Control.Feedback>}
-                </Form.Group>
-                <Form.Group>
-                    <Form.Control
-                        className="mb-4"
-                        type="password"
-                        placeholder="Password"
-                        name="password"
-                        
-                        // {...formik.getFieldProps('password')}
-                        onChange={formik.handleChange}
-                    />
-                    {formik.touched.password 
-                      && formik.errors.password 
-                      && 
-                    <Form.Control.Feedback type="invalid">
-                      {formik.errors.password}
-                    </Form.Control.Feedback>}
-                </Form.Group>
-                
-                <Button  className="mb-4 w-100 rounded" type="submit" variant="primary" disabled={formik.isSubmitting}>Sign In</Button>
-                <div className="text-center">
-                  <a href="/">Forgot password?</a>
+        <Formik
+        initialValues={{
+            email: '',
+            password: '',
+        }}
+        validationSchema={LoginSchema}
+        onSubmit={values => {
+            // same shape as initial values
+            console.log(values);
+        }}
+        >
+            {({ errors, touched }) => (
+                <div className="d-flex justify-content-center align-items-center vh-100">
+                    <div className="form-login">
+                        <h1 className="mb-5">Welcome back!</h1>
+                        <Form className="w-100">
+                            <div className="form-group mb-4">
+                                <Field className='form-control w-100'
+                                    type="text"
+                                    placeholder="Email"
+                                    name="email"
+                                    
+                                />
+                                {errors.email && touched.email && 
+                                <div className='text-danger'>
+                                {errors.email}
+                                </div>}
+                            </div>
+                            <div className='form-group mb-4'>
+                                <Field className='form-control w-100'
+                                    type="password"
+                                    placeholder="Password"
+                                    name="password"
+                                    
+                                />
+                                {errors.password && touched.password &&
+                                <div className='text-danger'>
+                                {errors.password}
+                                </div>}
+                            </div>
+                            
+                            <Button  className="mb-4 w-100 rounded" type="submit">Sign In</Button>
+                            <div className="text-center">
+                            <a href="/">Forgot password?</a>
+                            </div>
+                        </Form>
+                    </div>
                 </div>
-            </Form>
-        </div>
-      </div>
+            )}
+        </Formik>
     );
 }
